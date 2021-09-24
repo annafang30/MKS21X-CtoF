72a01584946fa6c1b2ea87f077184224b60cc1b6
Anna Fang <afang30@stuy.edu>
2021-09-23 22:49:55 -0400

    answered questions

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,3 +1,9 @@
+/* (originally written in a text file)
+1) the functions should have either a input representing celsious or an input representing fahrenheit.
+2) they should be booleans to account for possible decimals
+3) the functions should return either a celcius input calculated into fahrenheit or a fahrenheit input calculated into celcius*/
+
+
 public class CtoFTester{
   public static void main(String[] args) {
     System.out.println(celsiusToFahrenheit(30)); //tester lines
