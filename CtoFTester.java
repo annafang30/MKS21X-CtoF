ad3aff1868c3015eb513593a1df0e5e0c98286cd
Anna Fang <afang30@stuy.edu>
2021-09-23 22:39:34 -0400

    comments

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,9 +1,9 @@
 public class CtoFTester{
   public static void main(String[] args) {
-    System.out.println(celsiusToFahrenheit(30));
+    System.out.println(celsiusToFahrenheit(30)); //tester lines
     System.out.println(fahrenheitToCelsius(25.4));
   }
-  public static double celsiusToFahrenheit(double celsius){
+  public static double celsiusToFahrenheit(double celsius){ //so that decimals are included as well
     return ((celsius * (9/5)) + 32);
   }
   public static double fahrenheitToCelsius(double fahrenheit){
