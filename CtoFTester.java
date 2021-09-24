27484793b4514b7dbdec2635589350af493acca4
Anna Fang <afang30@stuy.edu>
2021-09-23 22:33:48 -0400

    fixed returns

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,11 +4,10 @@ public class CtoFTester{
     System.out.print(fahrenheitToCelsius(25.4));
   }
   public static double celsiusToFahrenheit(double celsius){
-    double ctfConversion = ((celsius * (9/5)) + 32);
-    return(ctfconversion);
+    return ((celsius * (9/5)) + 32);
   }
   public static double fahrenheitToCelsius(double fahrenheit){
-    double ftcConversion = ((fahrenheit - 32) * 5/9);
-    return ftcConversion;
+    return ((fahrenheit - 32) * 5/9);
+
   }
 }
