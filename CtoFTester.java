9d991e57beeac7b177c7911261b685ebf6b9ec52
Anna Fang <afang30@stuy.edu>
2021-09-23 22:27:44 -0400

    semicolons and testers

--- CtoFTester.java
+++ CtoFTester.java
@@ -1,12 +1,14 @@
 public class CtoFTester(){
   public static void main(String[] args) {
+    System.out.print(celsiusToFahrenheit(30));
+    System.out.print(fahrenheitToCelsius(25.4));
   }
   public static double celsiusToFahrenheit(double celsius){
-    double ctfConversion = (celsius * (9/5)) + 32
-    return(ctfconversion)
+    double ctfConversion = ((celsius * (9/5)) + 32);
+    return(ctfconversion);
   }
   public static double fahrenheitToCelsius(double fahrenheit){
-    double ftcConversion = (fahrenheit - 32) * 5/9
-    return ftcConversion
+    double ftcConversion = ((fahrenheit - 32) * 5/9);
+    return ftcConversion;
   }
 }
